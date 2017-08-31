bcbbf920097dcb16c46d17269022be2ed8ab758b
Matt Briney <mkbriney@gmail.com>
2017-08-31 15:22:32 -0400

    Comments API
    Grabs and cleans approved comments from Google.

--- comments.php
+++ comments.php
@@ -1,17 +1,25 @@
 <?php
+header('Content-Type: application/json');
 
+// Get remote Gooogle Spreadshet
 $data = file_get_contents("https://spreadsheets.google.com/feeds/list/1cJLHd3TsG9BeuHQ_0HaII1qhwRT9wOb2hsW2hY4-tKw/1/public/full?alt=json");
-
 $json = json_decode($data, true);
 
+// Setup array
+$comments = array();
 
+// Go through array and grab all entries that are approved
 foreach ($json["feed"]["entry"] as $entry) {
   if ($entry['gsx$approved']['$t'] == "x"){
-    echo $entry['gsx$yourmessage']['$t']."\n";
-    echo $entry['gsx$firstname']['$t']." ".substr($entry['gsx$lastname']['$t'],0,1)."."."\n";
-    echo "\n";
+
+    $item["comment"] = $entry['gsx$yourmessage']['$t'];
+    $item["name"] = $entry['gsx$firstname']['$t']." ".substr($entry['gsx$lastname']['$t'],0,1);
+
+    array_push($comments, $item);
   }
 }
 
-
+// Encode as JSON
+$output = json_encode($comments);
+echo $output;
 ?>
